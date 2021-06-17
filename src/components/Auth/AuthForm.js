39063d8d09f4efc5936764c42a5057a44d7ed7a6
harini <harinisiva1997@gmail.com>
2021-06-17 23:48:53 +0530

    adding user login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -17,35 +17,44 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
     setIsLoading(true)
+    let url
     if (isLogin) {
-
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + restApikey
     } else {
-
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + restApikey, {
-        method: 'POST',
-        body: JSON.stringify({
-          email: enteredEmail,
-          password: enteredPassword,
-          returnSecureToken: true
-        }),
-        headers: {
-          'content-type': 'application/json'
-        }
-      }).then(resp => {
-        if (resp.ok) {
-
-        } else {
-          return resp.json().then(data => {
-            // show error modal
-            let errorMessage = 'AUthentication failed'
-            if (data && data.error && data.error.message) {
-              errorMessage = data.error.message
-            }
-            alert(errorMessage)
-          })
-        }
-      })
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + restApikey
     }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      }),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(resp => {
+      if (resp.ok) {
+        return resp.json()
+      }
+      else {
+        return resp.json().then(data => {
+          // show error modal
+          let errorMessage = 'AUthentication failed'
+          if (data && data.error && data.error.message) {
+            errorMessage = data.error.message
+          }
+          // alert(errorMessage)
+          throw new Error(errorMessage)
+        })
+      }
+    })
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+        alert(err.message)
+      })
     setIsLoading(false)
   }
 
